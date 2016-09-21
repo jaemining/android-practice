9b34a4679f5ee5e7fb360e7ec2e0d944ec7f307d
Jaemin <jaemin0216@gmail.com>
2016-09-21 10:34:52 +0900

    change Theme to apply Material Design

--- app/src/main/java/com/jaemin/android/materialdesign_elevation/MainActivity.java
+++ app/src/main/java/com/jaemin/android/materialdesign_elevation/MainActivity.java
@@ -1,9 +1,16 @@
 package com.jaemin.android.materialdesign_elevation;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/*
+    1. res/values/style.xml의 Theme을 android:Theme.Material로 변경
+    2. AndroidManifest.xml의 application의 theme속성을 변경된 Theme으로 지정(Default로 됨)
+    3. Activity의 상속 클래스를 AppCompatActivity에서 Activity로 변경
+ */
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
