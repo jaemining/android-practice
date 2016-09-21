4e5bc3e032101cc63e1fb16eb30814a8aef313a0
Jaemin <jaemin0216@gmail.com>
2016-09-21 13:50:05 +0900

    initial commit

---
+++ app/src/main/java/com/jaemin/android/materialdesign_propertyanimation01/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jaemin.android.materialdesign_propertyanimation01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
