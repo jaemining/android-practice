33a893e7940568375495c00879ef4ba0a7893397
Jaemin <jaemin0216@gmail.com>
2016-09-21 13:07:42 +0900

    change code to be more simple

--- app/src/main/java/com/jaemin/android/materialdesign_viewanimation01/MainActivity.java
+++ app/src/main/java/com/jaemin/android/materialdesign_viewanimation01/MainActivity.java
@@ -21,41 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnAlpha = (Button)findViewById(R.id.button); // 전역변수인 btn을 사용 (자바 문법 구조)
-        btnAlpha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 1. 미리 정의된 애니매이션 xml을 load 한다
-                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.alpha);
-                // 2. 애니매이션을 뷰에 적용하고 실행한다
-                btnAlpha.startAnimation(animation);
-            }
-        });
-
         btnRotate = (Button)findViewById(R.id.btnRotate);
-        btnRotate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rotate);
-                btnRotate.startAnimation(animation);
-            }
-        });
-
         btnScale = (Button)findViewById(R.id.btnScale);
-        btnScale.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.scale);
-                btnScale.startAnimation(animation);
-            }
-        });
-
         btnTranslate = (Button)findViewById(R.id.btnTranslate);
-        btnTranslate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.translate);
-                btnTranslate.startAnimation(animation);
-            }
-        });
+
     }
+    public void onClickAlpha(View view) {
+        // 1. 미리 정의된 애니매이션 xml을 load 한다
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.alpha);
+        // 2. 애니매이션을 뷰에 적용하고 실행한다
+        btnAlpha.startAnimation(animation);
+    }
+
+    public void onClickRotate(View view) {
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rotate);
+        btnRotate.startAnimation(animation);
+    }
+
+    public void onClickScale(View view) {
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.scale);
+        btnScale.startAnimation(animation);
+    }
+
+    public void onClickTranslate(View view) {
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.translate);
+        btnTranslate.startAnimation(animation);
+    }
+
 }
