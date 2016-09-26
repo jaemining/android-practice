17dbac69caec04578cbc64c722b6ce7796fad00f
Jaemin <jaemin0216@gmail.com>
2016-09-27 08:59:21 +0900

    practice Layout Webview

--- app/src/main/java/com/jaemin/android/layoutwebview/MainActivity.java
+++ app/src/main/java/com/jaemin/android/layoutwebview/MainActivity.java
@@ -2,7 +2,10 @@ package com.jaemin.android.layoutwebview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        // 줌 사용
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(true);
+        // javascript
+        webSettings.setJavaScriptEnabled(true);
+
+
         webView.loadUrl("http://google.com");
     }
 }
