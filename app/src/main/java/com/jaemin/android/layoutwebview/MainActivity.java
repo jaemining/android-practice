a0c7d22a84d631494fcf1bb776ab77a84ac77b2e
Jaemin <jaemin0216@gmail.com>
2016-09-26 17:21:54 +0900

    practice WebView

---
+++ app/src/main/java/com/jaemin/android/layoutwebview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jaemin.android.layoutwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl("http://google.com");
+    }
+}
