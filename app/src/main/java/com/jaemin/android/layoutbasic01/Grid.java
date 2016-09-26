eed88a007bd9d90597b96969bbc1599fa5f9dfdd
Jaemin <jaemin0216@gmail.com>
2016-09-26 15:06:59 +0900

    practice Grid Layout

--- app/src/main/java/com/jaemin/android/layoutbasic01/Grid.java
+++ app/src/main/java/com/jaemin/android/layoutbasic01/Grid.java
@@ -3,18 +3,11 @@ package com.jaemin.android.layoutbasic01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Grid extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
-
-//        if(화면이 세로방향이면) {
-//
-//        } else {
-//            // 가로방향
-//        }
-
     }
 }
