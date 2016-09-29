0626de8ac1491f352a6c95a7e5c3eb92ce4202e4
Jaemin <jaemin0216@gmail.com>
2016-09-29 17:53:13 +0900

    practice Layout Manager

--- app/src/main/java/com/jaemin/android/basiclist/RecyclerCardActivity.java
+++ app/src/main/java/com/jaemin/android/basiclist/RecyclerCardActivity.java
@@ -2,8 +2,10 @@ package com.jaemin.android.basiclist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 
@@ -30,6 +32,16 @@ public class RecyclerCardActivity extends AppCompatActivity {
         RecyclerView listView = (RecyclerView) findViewById(R.id.recyclerCardView);
         RecyclerCardAdapter adapter = new RecyclerCardAdapter(datas, R.layout.activity_recycler_card_item, this);
         listView.setAdapter(adapter);
-        listView.setLayoutManager(new LinearLayoutManager(this));
+
+        // 수직 수평 스크롤이 가능
+        //RecyclerView.LayoutManager manager = new LinearLayoutManager(this);
+
+        // 그리드 타입
+        //RecyclerView.LayoutManager manager = new GridLayoutManager(this, 3);
+
+        // StaggeredGrid 타입
+        RecyclerView.LayoutManager manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+
+        listView.setLayoutManager(manager);
     }
 }
