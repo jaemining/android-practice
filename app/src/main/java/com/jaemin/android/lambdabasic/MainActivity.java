b3c0676c1b2eaa017659a078e0049026b4730d70
Jaemin <jaemin0216@gmail.com>
2016-09-30 11:42:47 +0900

    initial commit

---
+++ app/src/main/java/com/jaemin/android/lambdabasic/MainActivity.java
@@ -0,0 +1,23 @@
+package com.jaemin.android.lambdabasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("람다식으로 만들 예정");
+            }
+        });
+    }
+}
