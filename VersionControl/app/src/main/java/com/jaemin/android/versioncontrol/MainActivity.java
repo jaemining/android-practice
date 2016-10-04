f4225007c7bee65a4ed3663704c957e20d76c08f
Jaemin <jaemin0216@gmail.com>
2016-10-04 10:41:11 +0900

    Branch disposal by versions

--- VersionControl/app/src/main/java/com/jaemin/android/versioncontrol/MainActivity.java
+++ VersionControl/app/src/main/java/com/jaemin/android/versioncontrol/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jaemin.android.versioncontrol;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 버전별 분기 처리
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // sdk 버전이 마시멜로우일 경우
+
+        } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { // 롤리팝일 경우
+
+        } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) { // 킷캣일 경우
+
+        }
     }
 }
