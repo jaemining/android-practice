ccca1d2638b6adabcce868a6c91c37f5813ab918
Jaemin <jaemin0216@gmail.com>
2016-10-04 17:48:27 +0900

    Add permission check functions

--- MediaLibrary/app/src/main/java/com/jaemin/android/medialibrary/MainActivity.java
+++ MediaLibrary/app/src/main/java/com/jaemin/android/medialibrary/MainActivity.java
@@ -1,7 +1,12 @@
 package com.jaemin.android.medialibrary;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,19 +16,48 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static int REQUEST_CODE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            initData();
+        } else {
+            checkPermissions();
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermissions() {
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 쓰기 권한이 없으면 로직 처리
+            // 중간에 권한 내용에 대한 알림을 처리하는 함수
+            //shouldShowRequestPermissionRationale();
+            String permissionArray[] = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            requestPermissions(permissionArray, REQUEST_CODE);// 콜백함수가 호출될 때 requestCode가 넘어온다
+        } else {
+            // 쓰기 권한이 있으면 파일 생성
+            initData();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) { // 사용자가 권한 설정을 OK 했는지 확인하는 과정
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE :
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initData();
+                }
+        }
+    }
+
+    public void initData() {
         ArrayList<RecyclerData> datas = getMusicInfo();
-//        for (int i=0; i<100; i++) {
-//            RecyclerData data = new RecyclerData();
-//            data.title = (i+1) + " Make you feel my love";
-//            data.name = "adele";
-//
-//            datas.add(data);
-//        }
 
         RecyclerView listView = (RecyclerView) findViewById(R.id.recyclerView);
         RecyclerAdapter adapter = new RecyclerAdapter(datas, R.layout.recycler_item);
