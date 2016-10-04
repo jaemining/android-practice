f53fc16462bb64df4beb000d69614910955b57b9
Jaemin <jaemin0216@gmail.com>
2016-10-04 11:49:32 +0900

    Practice pixel conversion method

--- basic-list/app/src/main/java/com/jaemin/android/basiclist/ExpandableActivity.java
+++ basic-list/app/src/main/java/com/jaemin/android/basiclist/ExpandableActivity.java
@@ -1,9 +1,11 @@
 package com.jaemin.android.basiclist;
 
+import android.content.Context;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -57,6 +59,11 @@ public class ExpandableActivity extends AppCompatActivity {
 
         listView.setAdapter(ea);
 
+        // dp를 px로 변환할 때
+        int convertedPixel = (int)TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 200, getResources().getDisplayMetrics()
+        ); // 200 : dp 값 입력하는 부분 
+
     }
 
     public int getPixelFromDisplay(float pixels) {
@@ -64,5 +71,17 @@ public class ExpandableActivity extends AppCompatActivity {
         final float scale = getResources().getDisplayMetrics().density;
         // 컨버팅 dps > pixel - 화면 밀도 스케일을 기준으로
         return (int)(pixels * scale + 0.5f);
+    }
+
+    public int pxToDp(Context context, int px) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics(); // 실제 사이즈를 측정하기위해 metrics 이용
+        int dp = Math.round(px / (metrics.xdpi / metrics.DENSITY_DEFAULT));
+        return dp;
+    }
+
+    public int dpToPx(Context context, int dp) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int px = Math.round(dp * (metrics.xdpi / metrics.DENSITY_DEFAULT));
+        return px;
     }
 }
