08485294c261464a068a684d1d8be2b036216572
Jaemin <jaemin0216@gmail.com>
2016-10-05 14:57:02 +0900

    Add animation

--- FragmentBasic02/app/src/main/java/com/jaemin/android/fragmentbasic02/MainActivity.java
+++ FragmentBasic02/app/src/main/java/com/jaemin/android/fragmentbasic02/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setOne();
     }
 
-    // 처음에 들어오는 Fragment 지정 , 왜냐면 FrameLayout은 처음에 fragment를 지정해주는 기능이 없기때문 
+    // 처음에 들어오는 Fragment 지정 , 왜냐면 FrameLayout은 처음에 fragment를 지정해주는 기능이 없기때문
     public void setOne() {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
@@ -36,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager manager = getSupportFragmentManager(); // getSupportFragmentManage를 사용하는 이유 ?
         FragmentTransaction transaction = manager.beginTransaction();
 
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        
         // 쌓았을 때
         transaction.replace(R.id.fragment, fragmentTwo); // fragment를 넣을 장소를 먼저 지정해준다
         transaction.addToBackStack(null);
@@ -45,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
     public void goOne() {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
+
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+
         transaction.add(R.id.fragment, fragmentOne);
         transaction.addToBackStack(null);
         transaction.commit();
