a069f2379666efe9190010431698108f6b076a4d
Jaemin <jaemin0216@gmail.com>
2016-10-05 11:20:19 +0900

    Get phone book information

--- MediaLibrary_PhoneBook/app/src/main/java/com/jaemin/android/medialibrary_phonebook/MainActivity.java
+++ MediaLibrary_PhoneBook/app/src/main/java/com/jaemin/android/medialibrary_phonebook/MainActivity.java
@@ -5,6 +5,7 @@ import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Build;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -54,15 +55,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void initData() {
-        ArrayList<RecyclerData> datas = new ArrayList<>();
-        for (int i=0; i<100; i++) {
-
-            RecyclerData data = new RecyclerData();
-            data.name = "jaemin";
-            data.phoneNumber = "010 3126 4702";
-            datas.add(data);
-
-        }
+        ArrayList<RecyclerData> datas = getContacts();
 
         RecyclerView listView = (RecyclerView) findViewById(R.id.recyclerView);
         RecyclerAdapter adapter = new RecyclerAdapter(datas, R.layout.recycler_item);
@@ -70,53 +63,47 @@ public class MainActivity extends AppCompatActivity {
         listView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-//    public ArrayList<RecyclerData> getPhoneNumberInfo() {
-//        ArrayList<RecyclerData> datas = new ArrayList<>();
-//
-//        // 노래 아이디, 타이틀, 아티스트 이름
-//        String projections[] = {
-//                //MediaStore.Audio.Media._ID, MediaStore.Audio.Media.ALBUM_ID, MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ARTIST
-//                MediaStore.Files.
-//        };
-//
-//        //getContentResolver().query( uri 주소, 검색해올컬럼명들, 조건절, 조건절에매핑되는값, 정렬 );
-//        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projections, null, null, null);
-//        // 커서가 옮겨 가면서 데이터를 가져온다
-//        /*
-//            - uri : content://스키마 형태로 정해져 있는 곳의 데이터를 가져온다
-//            - projections : 가져올 컬럼 이름들의 배열. null을 입력하면 모든값을 가져온다(성능에 안좋을 수 있다)
-//            - selection : 조건절(where)에 해당하는 내용
-//            - selectionArgs : 조건절이 preparedstatement 형태일 때 ? 에 매핑되는 값의 배열
-//            - sort order : 정렬 조건
-//
-//            ** 이미지를 가져와야 할 땐 MediaStore.Image. ... 으로 하면 된다
-//         */
-//
-//        if(cursor != null) {
-//            // 커서를 다음 단계로 옮길 수 있으면 while문을 실행하라는 의미
-//            while (cursor.moveToNext()) {
-//                RecyclerData data = new RecyclerData();
-//                //data.name = String.valueOf(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
-//                // 데이터에 가수 이름 입력
-//                // 1. 가수 이름 컬럼의 순서(index)를 가져온다
-//                int artistIdx = cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
-//                // 2. 해당 index를 가진 컬럼의 실제값을 가져온다
-//                data.name = cursor.getString(artistIdx);
-//
-//                int idx = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
-//                data.title = cursor.getString(idx);
-//
-//                idx = cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID);
-//                data.albumId = cursor.getString(idx);
-//
-//                idx = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
-//                data.musicId = cursor.getString(idx);
-//
-//                datas.add(data);
-//            }
-//        }
-//        cursor.close();
-//
-//        return datas;
-//    }
+    public ArrayList<RecyclerData> getContacts() {
+        ArrayList<RecyclerData> datas = new ArrayList<>();
+
+        String projections[] = {
+                ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts._ID
+        };
+
+        // 조건절
+        // String selection = MediaStore.Audio.Media.TITLE + "!= '야생화' ";
+
+        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, projections, null, null, null);
+
+        if(cursor != null) {
+            while (cursor.moveToNext()) {
+                RecyclerData data = new RecyclerData();
+
+                // 주소록의 커서 사용
+                int idIndex = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+                String contactId = cursor.getString(idIndex); // 주소록 아이디 꺼냄 !
+
+                // 내부에서 커서를 돌려줘야한다!?!?!?
+                Cursor phoneCursor = getContentResolver().query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null, // 전화번호 데이터 셋이기 때문에 필요 없다. null을 날리면 전체를 가져온다
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId,
+                        null,
+                        null);
+
+                if(phoneCursor.moveToFirst()) {
+                    String tel = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    data.phoneNumber = tel;
+                }
+
+                phoneCursor.close();
+
+                data.name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                datas.add(data);
+            }
+        }
+        cursor.close();
+
+        return datas;
+    }
 }
