de1e07dfb33015520e5accc02f140580859faeff
Jaemin <jaemin0216@gmail.com>
2016-10-05 13:17:19 +0900

    Create fragment

---
+++ FragmentBasic01/app/src/main/java/com/jaemin/android/fragmentbasic01/FragmentOne.java
@@ -0,0 +1,29 @@
+package com.jaemin.android.fragmentbasic01;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentOne extends Fragment {
+
+    // 생성자 만들어준당
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+}
