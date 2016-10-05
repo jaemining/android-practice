54f86126616fac83bc59dfe9339b5ccfeba3eede
Jaemin <jaemin0216@gmail.com>
2016-10-05 14:38:21 +0900

    Set on click listener to switch fragment

--- FragmentBasic02/app/src/main/java/com/jaemin/android/fragmentbasic02/FragmentOne.java
+++ FragmentBasic02/app/src/main/java/com/jaemin/android/fragmentbasic02/FragmentOne.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -22,8 +23,18 @@ public class FragmentOne extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_one, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_fragment_one, container, false);
+        Button button = (Button) view.findViewById(R.id.button_goTwo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity mainActivity = (MainActivity) getActivity();
+                mainActivity.goTwo();
+            }
+        });
+
+        return view;
     }
 
 }
