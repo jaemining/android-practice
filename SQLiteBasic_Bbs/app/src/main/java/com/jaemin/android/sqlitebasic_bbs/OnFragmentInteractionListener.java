a148f7e618667b7b701a6fb6b281ee70df9a4a90
Jaemin <jaemin0216@gmail.com>
2016-10-10 17:59:51 +0900

    Create interface - OnFragmentInteractionListener

---
+++ SQLiteBasic_Bbs/app/src/main/java/com/jaemin/android/sqlitebasic_bbs/OnFragmentInteractionListener.java
@@ -0,0 +1,12 @@
+package com.jaemin.android.sqlitebasic_bbs;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Jaemin on 2016. 10. 10..
+ */
+
+public interface OnFragmentInteractionListener {
+    void OnFragmentInteraction(Fragment fragment);
+}
