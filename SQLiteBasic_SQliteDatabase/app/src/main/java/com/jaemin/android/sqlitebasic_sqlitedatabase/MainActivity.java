cbff57c4dd24f3eb53d17c56f8045a24a5c77b41
Jaemin <jaemin0216@gmail.com>
2016-10-10 16:48:53 +0900

    Practice sqlite database

--- SQLiteBasic_SQliteDatabase/app/src/main/java/com/jaemin/android/sqlitebasic_sqlitedatabase/MainActivity.java
+++ SQLiteBasic_SQliteDatabase/app/src/main/java/com/jaemin/android/sqlitebasic_sqlitedatabase/MainActivity.java
@@ -1,9 +1,14 @@
 package com.jaemin.android.sqlitebasic_sqlitedatabase;
 
 import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -15,19 +20,85 @@ import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView result;
+    Button openDatabase;
+    Button button_insert;
+    Button button_select;
+    Button button_update;
+    Button button_delete;
+
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        assetToDisk("sqlite.db");
+
+        init();
+
+        openDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db = SQLiteDatabase.openDatabase(getFullPath("sqlite.db"), null, 0);
+            }
+        });
+
+        button_insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(db != null) {
+                    // 쿼리를 실행해준다, 즉 select문을 제외한 모든 쿼리에 사용
+                    db.execSQL("insert into bbs(num, user_name, title) values(1, 'jaemin', 'title1');");
+                    // 쿼리 실행 후 결과값을 커서로 리턴해준다, 즉 select문에 사용
+                    //db.rawQuery("", null);
+                }
+            }
+        });
+
+        button_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(db != null) {
+                    Cursor cursor = db.rawQuery("select * from bbs", null);
+                    while(cursor.moveToNext()) {
+                        int idx = cursor.getColumnIndex("num");
+                        String num = cursor.getString(idx);
+                        idx = cursor.getColumnIndex("user_name");
+                        String user_name = cursor.getString(idx);
+                        idx = cursor.getColumnIndex("title");
+                        String title = cursor.getString(idx);
+                        idx = cursor.getColumnIndex("nDate");
+                        String nDate = cursor.getString(idx);
+                        String temp = result.getText().toString();
+                        result.setText(temp + "\n num = " + num + ", user_name = " + user_name + ", title = " + title + ", nDate = " + nDate);
+                    }
+                }
+            }
+        });
+    }
+
+    private void init() {
+        File file = new File(getFullPath("sqlite.db"));
+        if(!file.exists()) {
+            assetToDisk("sqlite.db");
+        }
+        result = (TextView) findViewById(R.id.editText);
+        openDatabase = (Button) findViewById(R.id.button_openDatabase);
+        button_insert = (Button) findViewById(R.id.button_insert);
+        button_select = (Button) findViewById(R.id.button_select);
+        button_update = (Button) findViewById(R.id.button_update);
+        button_delete = (Button) findViewById(R.id.button_delete);
+
+    }
+
+    public String getFullPath(String fileName) {
+        return getFilesDir().getAbsolutePath() + File.separator + fileName;
     }
 
     public void assetToDisk(String fileName) {
         // 외부에서 작성된 sqlite.db 파일 사용하기
         // 1. assets에 담아둔 파일을 internal 혹은 external 공간으로 복사하기 위해 읽어온다
         // 가져오는 명령어 - assetmanager
-        AssetManager manager = getAssets();
-
         InputStream is = null;
         BufferedInputStream bis = null;
         FileOutputStream fos = null;
@@ -35,13 +106,18 @@ public class MainActivity extends AppCompatActivity {
 
 
         try {
+            AssetManager manager = getAssets();
+
             is = manager.open(fileName);
             bis = new BufferedInputStream(is);
 
             // 2. 저장할 위치에 파일이 없으면 생성한다
-            String internalPath = getFilesDir().getAbsolutePath();
-            Log.i("aaa", "=== internalPath ===" + internalPath);
-            String targetFile = internalPath + File.separator + fileName;
+//            String internalPath = getFilesDir().getAbsolutePath();
+//            Log.i("aaa", "=== internalPath ===" + internalPath);
+//            String targetFile = internalPath + File.separator + fileName;
+
+            String targetFile = getFullPath(fileName);
+
             Log.i("aaa", "=== targetFile ===" + targetFile);
             File file = new File(targetFile);
             if(!file.exists()) {
