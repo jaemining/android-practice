63f7b707d523b7d348314124f688a679d93fb7de
Jaemin <jaemin0216@gmail.com>
2016-10-12 16:45:20 +0900

    Set dbHelper in MainActivity

--- SQLiteBasic_DbHelper/app/src/main/java/com/jaemin/android/sqlitebasic_dbhelper/MainActivity.java
+++ SQLiteBasic_DbHelper/app/src/main/java/com/jaemin/android/sqlitebasic_dbhelper/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jaemin.android.sqlitebasic_dbhelper;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase db = DBHelper.openDatabase(this);
+        Log.i("aaaa", "Db === "+db);
+
+        Cursor cursor = db.rawQuery("select title from bbs4", null);
+        while (cursor.moveToNext()) {
+            Log.i("aaaa", "title ==== "+cursor.getString(0));
+        }
+
+
+        cursor.close();
+        db.close();
     }
 }
