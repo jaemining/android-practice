41222d081a286509bcaadbf859493d175c3e90a3
Jaemin <jaemin0216@gmail.com>
2016-10-13 10:25:06 +0900

    Initial commit - SQLite_Memopad

---
+++ SQLite_Memopad/app/src/main/java/com/jaemin/android/sqlite_memopad/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jaemin.android.sqlite_memopad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
