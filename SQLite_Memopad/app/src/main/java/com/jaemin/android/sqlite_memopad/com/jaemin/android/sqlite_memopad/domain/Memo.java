677b037ea6aa221305392a953b38ef5ee5fe004d
Jaemin <jaemin0216@gmail.com>
2016-10-13 14:05:34 +0900

    Make domain package and Make Memo class in domain package

---
+++ SQLite_Memopad/app/src/main/java/com/jaemin/android/sqlite_memopad/com/jaemin/android/sqlite_memopad/domain/Memo.java
@@ -0,0 +1,13 @@
+package com.jaemin.android.sqlite_memopad.com.jaemin.android.sqlite_memopad.domain;
+
+/**
+ * Created by Jaemin on 2016. 10. 13..
+ */
+
+public class Memo {
+
+    public int no;
+    public String contents;
+    public long ndate;
+
+}
