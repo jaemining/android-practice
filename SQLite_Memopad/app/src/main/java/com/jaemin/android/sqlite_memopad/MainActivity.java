d426a25d459f67eb3e121d45689421f779f93aa9
Jaemin <jaemin0216@gmail.com>
2016-10-13 14:07:04 +0900

    Set DBHelper

--- SQLite_Memopad/app/src/main/java/com/jaemin/android/sqlite_memopad/MainActivity.java
+++ SQLite_Memopad/app/src/main/java/com/jaemin/android/sqlite_memopad/MainActivity.java
@@ -2,6 +2,11 @@ package com.jaemin.android.sqlite_memopad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.jaemin.android.sqlite_memopad.com.jaemin.android.sqlite_memopad.domain.Memo;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBHelper dbHelper = new DBHelper(this);
+        Memo memo = new Memo();
+        memo.contents = "메모 내용 ";
+        memo.ndate = dbHelper.getTimeStamp();
+
+        dbHelper.dbInsert(memo);
+        Log.i("Memo", "Db ===== "+dbHelper);
+
+
+        ArrayList<Memo> datas = dbHelper.dbSelectAll();
+        for (Memo data : datas) {
+            Log.i("Memo", "no ===== " + data.no);
+            Log.i("Memo", "contents ===== " + data.contents);
+            Log.i("Memo", "ndate ===== " + data.ndate);
+        }
     }
 }
