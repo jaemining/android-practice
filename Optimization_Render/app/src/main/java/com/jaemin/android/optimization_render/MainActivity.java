0b70595c3b345233b6448a2f155e31efc7e3e82c
Jaemin <jaemin0216@gmail.com>
2016-10-13 20:27:17 +0900

    Add debug code

--- Optimization_Render/app/src/main/java/com/jaemin/android/optimization_render/MainActivity.java
+++ Optimization_Render/app/src/main/java/com/jaemin/android/optimization_render/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jaemin.android.optimization_render;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Debug.startMethodTracing("traceResult");
+        print100000();
+    }
+
+    public void print100000() {
+        for (int i=0; i<100000; i++) {
+           Log.i("PerformanceTest", "i = " + i);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Debug.stopMethodTracing();
     }
 }
