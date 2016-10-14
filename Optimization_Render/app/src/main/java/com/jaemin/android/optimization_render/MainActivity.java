42452a2d07f9a0233ce56cb4e9b71321b41475e8
Jaemin <jaemin0216@gmail.com>
2016-10-14 11:05:19 +0900

    Add two thread

--- Optimization_Render/app/src/main/java/com/jaemin/android/optimization_render/MainActivity.java
+++ Optimization_Render/app/src/main/java/com/jaemin/android/optimization_render/MainActivity.java
@@ -12,13 +12,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Debug.startMethodTracing("traceResult");
-        print100000();
+        Debug.startMethodTracing("traceResult_20161014");
+
+        Thread another01 = new Thread() {
+            @Override
+            public void run() {
+                // 다른 Thread에서 돌아가는 로직
+                print1000("another01");
+            }
+        };
+
+        another01.start(); //  main thread가 아닌 다른 thread를 만들어 로직을 실행한다
+
+        Thread another02 = new Thread() {
+            @Override
+            public void run() {
+                // 다른 Thread에서 돌아가는 로직
+                print1000("another02");
+            }
+        };
+
+        another02.start();
+
+        print1000("main");
     }
 
-    public void print100000() {
-        for (int i=0; i<100000; i++) {
-           Log.i("PerformanceTest", "i = " + i);
+    public void print1000(String tag) {
+        for (int i=0; i<1000; i++) {
+           Log.i("PerformanceTest", tag + "i = " + i);
         }
     }
 
