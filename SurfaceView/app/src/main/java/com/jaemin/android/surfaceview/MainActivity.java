5fb18af68fd3c8770e949f94528c8d289f17d9da
Jaemin <jaemin0216@gmail.com>
2016-10-20 16:18:13 +0900

    Initial commit - SurfaceView

---
+++ SurfaceView/app/src/main/java/com/jaemin/android/surfaceview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jaemin.android.surfaceview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
