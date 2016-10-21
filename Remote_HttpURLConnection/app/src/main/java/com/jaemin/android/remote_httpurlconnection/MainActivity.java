f317275f86cbe802e3986b6e37783abb5ff3b255
Jaemin <jaemin0216@gmail.com>
2016-10-21 15:24:01 +0900

    Call google.com

--- Remote_HttpURLConnection/app/src/main/java/com/jaemin/android/remote_httpurlconnection/MainActivity.java
+++ Remote_HttpURLConnection/app/src/main/java/com/jaemin/android/remote_httpurlconnection/MainActivity.java
@@ -1,13 +1,62 @@
 package com.jaemin.android.remote_httpurlconnection;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getNaver();
+            }
+        });
+
+    }
+
+    private void getNaver() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                String result = "";
+                try {
+                    result = Remote.getData("http://google.com");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                tv.setText(s); // doInBackground에서 넘어오는 값을 s로 받는다
+
+            }
+        }.execute();
+
+
     }
 }
