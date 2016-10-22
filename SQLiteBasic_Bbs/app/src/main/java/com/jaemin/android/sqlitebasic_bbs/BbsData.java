07c0e0ef877adbc3e2e55c1f8131256cf198c5ab
Jaemin <jaemin0216@gmail.com>
2016-10-22 23:41:52 +0900

    Create BbsData class

---
+++ SQLiteBasic_Bbs/app/src/main/java/com/jaemin/android/sqlitebasic_bbs/BbsData.java
@@ -0,0 +1,13 @@
+package com.jaemin.android.sqlitebasic_bbs;
+
+/**
+ * Created by Jaemin on 2016. 10. 11..
+ */
+
+public class BbsData {
+    public int no;
+    public String title;
+    public String name;
+    public String contents;
+    public String ndate;
+}
