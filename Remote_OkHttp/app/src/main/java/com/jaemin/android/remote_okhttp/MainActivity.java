b377ad832bb9efdfd8552865df2948b69eb2d504
Jaemin <jaemin0216@gmail.com>
2016-10-26 15:59:40 +0900

    Practice okHttp

--- Remote_OkHttp/app/src/main/java/com/jaemin/android/remote_okhttp/MainActivity.java
+++ Remote_OkHttp/app/src/main/java/com/jaemin/android/remote_okhttp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.jaemin.android.remote_okhttp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String key = "6d527467476a61653639544f446167";
+        String serviceName = "CardSubwayStatisticsService";
+
+        int begin = 1; // 시작 위치
+        int end = 5;
+
+        String url = "http://openapi.seoul.go.kr:8088/"+key+"/json/"+serviceName+"/"+begin+"/"+end+"/201306/";
+
+        callHttp(url);
     }
+
+    public void callHttp(String url) {
+        new AsyncTask<String, Void, String>() {
+
+            @Override
+            protected String doInBackground(String... params) {
+                String result = "";
+                try {
+                    result = getData(params[0]);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                Log.e("Result", result);
+            }
+
+        }.execute(url);
+    }
+
+    public String getData(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(url).build();
+
+        Response response = client.newCall(request).execute();
+
+        return response.body().string();
+    }
+
 }
