4f7617df963113ab49fbe48eee3655f2aae26b2e
Jaemin <jaemin0216@gmail.com>
2016-10-31 13:31:38 +0900

    Create MyFirebaseMessagingService.java extends FirebaseMessagingService

---
+++ Firebase_Message/app/src/main/java/com/jaemin/android/firebase_message/MyFirebaseMessagingService.java
@@ -0,0 +1,16 @@
+package com.jaemin.android.firebase_message;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Jaemin on 2016. 10. 31..
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
