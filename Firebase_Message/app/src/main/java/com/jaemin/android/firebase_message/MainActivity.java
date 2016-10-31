7dc0a34b03dbece1736e3c10b6706c8bd6690793
Jaemin <jaemin0216@gmail.com>
2016-10-31 15:07:06 +0900

    Practice firebase messaging

--- Firebase_Message/app/src/main/java/com/jaemin/android/firebase_message/MainActivity.java
+++ Firebase_Message/app/src/main/java/com/jaemin/android/firebase_message/MainActivity.java
@@ -4,8 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     Button btnToken;
 
--- Firebase_Message/app/src/main/java/com/jaemin/android/firebase_message/MyFirebaseInstanceIDService.java
+++ Firebase_Message/app/src/main/java/com/jaemin/android/firebase_message/MyFirebaseInstanceIDService.java
@@ -5,19 +5,20 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-import static android.content.ContentValues.TAG;
-
 /**
  * Created by Jaemin on 2016. 10. 31..
  */
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService{
 
+    private static final String TAG = "MyFirebaseIIDService";
+
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken); // 토큰을 기준으로 폰에 메세지를 날린다
+
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on     the server side, send the
