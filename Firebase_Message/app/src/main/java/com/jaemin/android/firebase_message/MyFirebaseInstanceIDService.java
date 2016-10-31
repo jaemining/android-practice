fded44379409c8ebe2048187721060e00b772a8c
Jaemin <jaemin0216@gmail.com>
2016-10-31 13:58:14 +0900

    Add comment

--- Firebase_Message/app/src/main/java/com/jaemin/android/firebase_message/MyFirebaseInstanceIDService.java
+++ Firebase_Message/app/src/main/java/com/jaemin/android/firebase_message/MyFirebaseInstanceIDService.java
@@ -17,10 +17,10 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService{
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        Log.d(TAG, "Refreshed token: " + refreshedToken); // 토큰을 기준으로 폰에 메세지를 날린다
 
         // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
+        // manage this apps subscriptions on     the server side, send the
         // Instance ID token to your app server.
         // sendRegistrationToServer(refreshedToken);
     }
