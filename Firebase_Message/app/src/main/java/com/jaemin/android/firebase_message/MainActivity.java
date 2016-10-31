48e804102ecee988d34003af555b4fba9b294feb
Jaemin <jaemin0216@gmail.com>
2016-10-31 13:59:14 +0900

    Connect code on button

--- Firebase_Message/app/src/main/java/com/jaemin/android/firebase_message/MainActivity.java
+++ Firebase_Message/app/src/main/java/com/jaemin/android/firebase_message/MainActivity.java
@@ -2,12 +2,26 @@ package com.jaemin.android.firebase_message;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnToken = (Button) findViewById(R.id.button_generateToken);
+
+        btnToken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyFirebaseInstanceIDService service = new MyFirebaseInstanceIDService();
+                service.onTokenRefresh();
+            }
+        });
     }
 }
