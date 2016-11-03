ca9e44990a198d7403b0110fd8312d3de80fbc0d
Jaemin <jaemin0216@gmail.com>
2016-11-03 14:52:51 +0900

    Create observable and Check log

--- RxAndroid_Basic/app/src/main/java/com/jaemin/android/rxandroid_basic/MainActivity.java
+++ RxAndroid_Basic/app/src/main/java/com/jaemin/android/rxandroid_basic/MainActivity.java
@@ -2,6 +2,10 @@ package com.jaemin.android.rxandroid_basic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1. Observable을 생성
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello RxAndroid ~!");
+
+            }
+        });
+
+        // 2. Observable을 통해 데이터를 가져온다
+        observable.subscribe(new Subscriber<String>() { // call()이 호출된다
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.i("RxAndroid", "next value === " + s);
+            }
+        });
     }
 }
