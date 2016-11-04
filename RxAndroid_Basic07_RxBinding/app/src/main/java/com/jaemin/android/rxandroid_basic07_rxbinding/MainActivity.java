240a5ed2b741df2dc21640acd5883ebef293430b
Jaemin <jaemin0216@gmail.com>
2016-11-04 16:18:43 +0900

    Practice RxBinding

--- RxAndroid_Basic07_RxBinding/app/src/main/java/com/jaemin/android/rxandroid_basic07_rxbinding/MainActivity.java
+++ RxAndroid_Basic07_RxBinding/app/src/main/java/com/jaemin/android/rxandroid_basic07_rxbinding/MainActivity.java
@@ -2,6 +2,16 @@ package com.jaemin.android.rxandroid_basic07_rxbinding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jakewharton.rxbinding.view.RxView;
+import com.jakewharton.rxbinding.widget.RxTextView;
+
+import java.util.Random;
+
+import rx.Observable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RxView
+                .clicks(findViewById(R.id.button_bind))
+                .map(event -> new Random().nextInt())
+                .subscribe(
+                        rand -> ((TextView) findViewById(R.id.textView)).setText("value = " + rand)
+                );
+
+        Observable<String> leftObs =
+                RxView
+                .clicks(findViewById(R.id.button_left))
+                .map(event -> "left");
+
+        Observable<String> rightObs =
+                RxView
+                .clicks(findViewById(R.id.button_right))
+                .map(event -> "right");
+
+        Observable.merge(leftObs, rightObs)
+                .subscribe(
+                  text -> Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+                );
+
+        // text change event
+        RxTextView.textChangeEvents((TextView)findViewById(R.id.editText_inputWord))
+                .subscribe(
+                        word -> Log.i("RxBinding", "search / word : " + word.text().toString())
+                );
     }
 }
