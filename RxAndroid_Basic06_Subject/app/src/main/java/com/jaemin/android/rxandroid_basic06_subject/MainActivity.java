266d044ca464558293f28d8876aed1e39e671af5
Jaemin <jaemin0216@gmail.com>
2016-11-04 14:36:19 +0900

    Initial commit - RxAndroid_Basic06_Subject

---
+++ RxAndroid_Basic06_Subject/app/src/main/java/com/jaemin/android/rxandroid_basic06_subject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jaemin.android.rxandroid_basic06_subject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
