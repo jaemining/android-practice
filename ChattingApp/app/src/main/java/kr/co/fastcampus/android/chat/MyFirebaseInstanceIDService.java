e07971fc0eb3a38bbce54b33bd75e66f7f829b22
Jaemin <jaemin0216@gmail.com>
2016-11-07 17:23:10 +0900

    Create MyFirebaseInstanceIDService.java extends FirebaseInstanceIdService

---
+++ ChattingApp/app/src/main/java/kr/co/fastcampus/android/chat/MyFirebaseInstanceIDService.java
@@ -0,0 +1,50 @@
+package kr.co.fastcampus.android.chat;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+/**
+ * Created by Jaemin on 2016. 11. 7..
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "MyFirebaseIDService";
+    public static final String TOPIC = "update";
+
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC);
+    }
+    // [END refresh_token]
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+
+}
