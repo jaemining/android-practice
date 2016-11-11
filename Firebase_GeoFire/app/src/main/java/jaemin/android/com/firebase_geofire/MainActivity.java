8497b00c2c0fb3f6166afa4736d894ed0728d5e9
Jaemin <jaemin0216@gmail.com>
2016-11-11 14:21:47 +0900

    Initial commit - Firebase_GeoFire

---
+++ Firebase_GeoFire/app/src/main/java/jaemin/android/com/firebase_geofire/MainActivity.java
@@ -0,0 +1,13 @@
+package jaemin.android.com.firebase_geofire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
